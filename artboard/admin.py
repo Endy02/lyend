1fa86c6507656149e1d3d66b1135a80ff4b375b8
fujyn <endy.alexis@outlook.com>
2021-05-23 06:11:09 +0200

    Artboards Model creation and admin set up

--- artboard/admin.py
+++ artboard/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.db import models
+from .models import Artboard, Color, Category
 
 # Register your models here.
+
+class ColorAdmin(admin.ModelAdmin):
+    list_display= ('name', 'hex', 'rgb', 'created_at')
+    ordering = ("-created_at")
+
+@admin.register(Artboard)
+class ArtboardAdmin(admin.ModelAdmin):
+    list_display= ('name', 'category', 'created_at')
+    ordering =("-created_at",)
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display =('title', 'created_at')
+    ordering = ("-created_at",)
--- artboard/models.py
+++ artboard/models.py
@@ -1,3 +1,44 @@
+from django.db.models.signals import m2m_changed
+from django.core.exceptions import ValidationError
 from django.db import models
+from .utils import get_colour_name
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=100)
+    hex = models.CharField(max_length=10)
+    rgb = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        hex = self.hex.lstrip('#')
+        hlen = len(hex)
+        self.rgb = tuple(int(hex[i:i+hlen/3], 16) / 255.0 for i in range(0, hlen, hlen/3))
+        self.name = get_colour_name(self.rgb)
+        return super(Color, self).save(*args, **kwargs)
+    
+    
+class Artboard(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    colors = models.ManyToManyField(Color)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+def artboards_changed(sender, **kwargs):
+    if kwargs['instance'].regions.count() > 5:
+        raise ValidationError("You can't assign more than five colors to an artboard")
+    
+
+m2m_changed.connect(artboards_changed, sender=Artboard.colors)
