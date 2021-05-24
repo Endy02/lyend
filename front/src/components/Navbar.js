3e2421860910bfba844259ccd0ad0b3ab6411386
fujyn <endy.alexis@outlook.com>
2021-05-25 00:31:20 +0200

    Update Navbar Component

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import Button from './Button';
 
 
@@ -31,10 +31,10 @@ const Navbar = () => {
                     <i className={click ? 'fas fa-times fa-lg' : 'fas fa-bars fa-lg'} />
                 </div>
                 <ul className={click ? "nav-list active" : "nav-list"}>
-                    <li className="nav-item"><Link to='/artboards' className='nav-link' onClick={closeMobileMenu}>Artboards</Link></li>
-                    <li className="nav-item"><Link to='/gradients' className='nav-link' onClick={closeMobileMenu}>Gradients</Link></li>
-                    <li className="nav-item"><Link to='/armony' className='nav-link' onClick={closeMobileMenu}>Armony</Link></li>
-                    <li className="nav-item"><Link to='/sign-up' className='nav-link-mobile' onClick={closeMobileMenu}>Sign Up</Link></li>
+                    <li className="nav-item"><NavLink to='/artboards' activeClassName="active" className="nav-link" onClick={closeMobileMenu}>Artboards</NavLink></li>
+                    <li className="nav-item"><NavLink to='/gradients' activeClassName="active" className="nav-link" onClick={closeMobileMenu}>Gradients</NavLink></li>
+                    <li className="nav-item"><NavLink to='/armony' activeClassName="active" className="nav-link" onClick={closeMobileMenu}>Armony</NavLink></li>
+                    <li className="nav-item"><Link to='/sign-up' className="nav-link-mobile" onClick={closeMobileMenu}>Sign Up</Link></li>
                 </ul>
                 {button && <Button buttonStyle='btn-current' link='/sign-up'>Sign up</Button>}
             </header>
